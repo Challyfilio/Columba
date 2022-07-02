2be7653156f7281dcb1f8ba65731948158066b12
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2022-07-02 10:48:13 +0800

    7.2

--- scripts/eval_all.sh
+++ scripts/eval_all.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 bash eval.sh resnet50 aircraft 0 0 100
-bash eval.sh resnet50 aircraft 1 0 100
+bash eval.sh resnet50 aircraft 1 0 15
 bash eval.sh resnet50 aircraft 1 1 100
 
 bash eval.sh resnet18 aircraft 0 0 100
-bash eval.sh resnet18 aircraft 1 0 100
+bash eval.sh resnet18 aircraft 1 0 15
 bash eval.sh resnet18 aircraft 1 1 100
 
-bash eval.sh resnet50 aircraft_similar 0 0 100
-bash eval.sh resnet50 aircraft_similar 1 0 100
+bash eval.sh resnet50 aircraft_similar 0 0 15
+bash eval.sh resnet50 aircraft_similar 1 0 15
 bash eval.sh resnet50 aircraft_similar 1 1 100
 
-bash eval.sh resnet18 aircraft_similar 0 0 100
-bash eval.sh resnet18 aircraft_similar 1 0 100
+bash eval.sh resnet18 aircraft_similar 0 0 15
+bash eval.sh resnet18 aircraft_similar 1 0 15
 bash eval.sh resnet18 aircraft_similar 1 1 100
